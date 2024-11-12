1ccf92ee27fca2ee68e249cc2edb71481cbb4fbc
reosfire <2.com9761721@gmail.com>
2024-11-12 17:56:26 +0300

    some changes before it is done

--- lab3/src/sequential_runner.c
+++ lab3/src/sequential_runner.c
@@ -7,5 +7,21 @@
 #include <sys/wait.h>
 
 int main() {
-  execl("sequential_min_max", "sequential_min_max", "21", "10", NULL);
+  int pid = fork();
+  if (pid < 0) {
+    printf("cannot fork\n");
+  } else if(pid == 0) {
+    execl("sequential_min_max", "sequential_min_max", "21", "10", NULL);
+  }
+
+  printf("Waiting subprocess..\n");
+  int waitedProcess = wait(NULL);
+  if (waitedProcess < 0) {
+      printf("Can't wait subprocess!\n");
+      return -1;
+  } else {
+    printf("Wait success!\n");
+  }
+
+  return 0;
 }
